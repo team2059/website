9fbf625494627573212a8e949ae31820b67cf7b5
Carl Colglazier <carl@carlcolglazier.com>
2015-06-01 10:43:24 -0400

    Add 'watch' command to gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -7,26 +7,38 @@ var gulp = require('gulp'),
     LessPluginCleanCSS = require('less-plugin-clean-css'),
     cleancss = new LessPluginCleanCSS({ advanced: true });
 
+var paths = {
+    less: 'public/**/*.less',
+    html: './public/**/*.html',
+    dest: './public',
+    less_dest: './public/css'
+};
+
 gulp.task('less', function () {
     var DEST = './public/css';
     return gulp.src('./public/css/*.less')
-        .pipe(changed(DEST))
+        .pipe(changed(paths.less_dest))
         .pipe(less({
             plugins: [cleancss]
         }))
-        .pipe(gulp.dest(DEST));
+        .pipe(gulp.dest(paths.less_dest));
 });
 
 gulp.task('minify-html', function () {
-    var DEST = './public';
-    return gulp.src('./public/**/*.html')
+    return gulp.src()
         .pipe(minifyHtml({}))
-        .pipe(gulp.dest(DEST));
+        .pipe(gulp.dest(paths.dest));
 });
 
 gulp.task('clean', function () {
-    return gulp.src('public/**/*.less', {read: false})
+    return gulp.src(paths.less, {read: false})
         .pipe(clean());
 });
 
+gulp.task('watch', function () {
+    gulp.watch(paths.less, ['less', 'clean']);
+    gulp.watch(paths.html, ['minify-html']);
+});
+
 gulp.task('default', ['less', 'clean', 'minify-html']);
+gulp.task('develop', ['watch']);
