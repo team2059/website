156b9ae467b76500a0adab9dbda4d8c5873212e7
Carl Colglazier <carl@carlcolglazier.com>
2015-06-03 08:05:04 -0400

    Update gulpfile.
    + Use browser-sync during development in union with hugo to simplify
      the front end development process.
    + Run certain tasks synchronously.

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,76 @@
 var gulp = require('gulp'),
-    changed = require('gulp-changed'),
+    runSequence = require('run-sequence'),
     less = require('gulp-less'),
     clean = require('gulp-clean'),
     minifyHtml = require('gulp-minify-html'),
+    run = require('gulp-run'),
     path = require('path'),
     LessPluginCleanCSS = require('less-plugin-clean-css'),
-    cleancss = new LessPluginCleanCSS({ advanced: true });
+    cleancss = new LessPluginCleanCSS({ advanced: true }),
+    browserSync = require('browser-sync').create(),
+    reload = browserSync.reload;
 
 var paths = {
-    less: 'public/**/*.less',
+    less: './public/css/*.less',
     html: './public/**/*.html',
+    hugo: ['./content/**/*', './layouts/**/*', './static/**/*'],
     dest: './public',
     less_dest: './public/css'
 };
 
+gulp.task('run-hugo', function() {
+    var base_url = (process.env.BASE_URL) ? ' -b ' + process.env.BASE_URL : '';
+    return run('hugo -D' + base_url).exec()
+        .pipe(reload({
+            stream: true
+        }));
+});
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./public",
+        },
+        port: 2059
+    });
+});
+
+gulp.task('clean', function () {
+    return gulp.src(paths.less, {
+        read: false
+    })
+        .pipe(clean());
+});
+
 gulp.task('less', function () {
     var DEST = './public/css';
-    return gulp.src('./public/css/*.less')
-        .pipe(changed(paths.less_dest))
+    return gulp.src('./public/css/style.less')
         .pipe(less({
             plugins: [cleancss]
         }))
-        .pipe(gulp.dest(paths.less_dest));
+        .pipe(gulp.dest(paths.less_dest))
+        .pipe(reload({
+            stream: true
+        }));
 });
 
 gulp.task('minify-html', function () {
-    return gulp.src()
+    return gulp.src(paths.html)
         .pipe(minifyHtml({}))
         .pipe(gulp.dest(paths.dest));
 });
 
-gulp.task('clean', function () {
-    return gulp.src(paths.less, {read: false})
-        .pipe(clean());
-});
-
 gulp.task('watch', function () {
-    gulp.watch(paths.less, ['less', 'clean']);
-    gulp.watch(paths.html, ['minify-html']);
+    gulp.watch(paths.hugo, ['prepare']);
 });
 
-gulp.task('default', ['less', 'clean', 'minify-html']);
-gulp.task('develop', ['watch']);
+gulp.task('build', function (callback) {
+    runSequence(['minify-html', 'less'], 'clean', callback);
+});
+
+gulp.task('prepare', function (callback) {
+    runSequence('run-hugo', 'build', callback);
+});
+
+gulp.task('default', ['build']);
+gulp.task('develop', ['prepare', 'watch', 'browser-sync']);
